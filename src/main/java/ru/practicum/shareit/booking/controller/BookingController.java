1f46fac898ecf2c56e7e77d00260a8a2082193b7
Владислав Г. <vlad65253@gmail.com>
2025-03-14 13:12:16 +0300

    Выполнение ФЗ15 (v1.0) Соединение с базой, разработка нового функционала бронирования и комментариев

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -20,6 +20,7 @@ public class BookingController {
     public BookingDto createBooking(@RequestHeader(HEADER_USER_ID) Long userId, @RequestBody CreateBookingDto createBookingDto) {
         return bookingService.addBooking(userId, createBookingDto);
     }
+
     @PatchMapping("/{bookingId}")
     public BookingDto patchBooking(@RequestParam boolean approved, @RequestHeader(HEADER_USER_ID) long userId, @PathVariable long bookingId) {
         return bookingService.patchBooking(bookingId, approved, userId);
@@ -29,12 +30,14 @@ public class BookingController {
     public List<BookingDto> getBookingsForBookerId(@RequestHeader(HEADER_USER_ID) Long userId, @RequestParam(defaultValue = "ALL") State state) {
         return bookingService.getBookingsForBookerIdAndState(userId, state);
     }
+
     @GetMapping("/owner")
     public List<BookingDto> getBookingsForOwnerId(@RequestHeader(HEADER_USER_ID) Long userId, @RequestParam(defaultValue = "ALL") State state) {
         return bookingService.getBookingsByOwnerIdAndState(userId, state);
     }
+
     @GetMapping("/{bookingId}")
-    public BookingDto getBookingsByBookerOrOwnerItem(@RequestHeader("X-Sharer-User-Id") Long userId, @PathVariable Long bookingId) {
+    public BookingDto getBookingsByBookerOrOwnerItem(@RequestHeader(HEADER_USER_ID) Long userId, @PathVariable Long bookingId) {
         return bookingService.getBookingsByOwnerAndBooker(userId, bookingId);
     }
 
